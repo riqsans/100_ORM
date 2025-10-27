e9d31ed26f778fa398583079a5987aaccd097ba0
riqsans <thrq88@gmail.com>
2025-10-27 09:26:33 +0700

    menambahkan isi file index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false 
+}));
+
+app.listen(port, () => {
+    console.log('server started on port 3000');
+})
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server Started');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
